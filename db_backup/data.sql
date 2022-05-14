bf76c5b5c0d199085e043a30370e4cf219a2ef9b
ThoiPT <lephatloc2016@gmail.com>
2022-05-14 13:41:46 +0700

    update database - change name request__skills to request_skill

--- db_backup/data.sql
+++ db_backup/data.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: fsoft_data
+-- Host: localhost    Database: tracking_data
 -- ------------------------------------------------------
 -- Server version	10.4.24-MariaDB
 
@@ -56,7 +56,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (5,'2022_04_27_102535_create_roles_table',2),(6,'2022_05_03_141234_create_skills_table',2),(7,'2022_05_03_145659_add_new',2),(12,'2014_10_12_000000_create_users_table',3),(13,'2014_10_12_100000_create_password_resets_table',3),(14,'2019_08_19_000000_create_failed_jobs_table',3),(15,'2019_12_14_000001_create_personal_access_tokens_table',3);
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2019_12_14_000001_create_personal_access_tokens_table',1),(5,'2022_05_12_155608_create_skills_table',2),(6,'2022_05_13_012728_create_requests_table',3),(7,'2022_05_13_165343_create_request__skills_table',4);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,29 +126,92 @@ LOCK TABLES `personal_access_tokens` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `roles`
+-- Table structure for table `request_skill`
 --
 
-DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `request_skill`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roles` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `roleName` int(11) NOT NULL,
-  `roleDes` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE `request_skill` (
+  `request_id` bigint(20) unsigned DEFAULT NULL,
+  `skill_id` bigint(20) unsigned DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `roles`
+-- Dumping data for table `request_skill`
 --
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+LOCK TABLES `request_skill` WRITE;
+/*!40000 ALTER TABLE `request_skill` DISABLE KEYS */;
+INSERT INTO `request_skill` VALUES (1,2,NULL,NULL),(1,3,NULL,NULL),(5,1,'2022-05-13 23:26:03','2022-05-13 23:26:03'),(5,2,'2022-05-13 23:26:03','2022-05-13 23:26:03'),(5,3,'2022-05-13 23:26:03','2022-05-13 23:26:03');
+/*!40000 ALTER TABLE `request_skill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `experience` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numRecruit` int(11) NOT NULL,
+  `open` date NOT NULL,
+  `close` date NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `skill_id` bigint(20) unsigned NOT NULL,
+  `group_id` bigint(20) unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (1,'Tuyển dụng Full Stack','1 Year','Fresher',10,'2022-05-14','2022-05-24','<p><strong>Tuyển dụng FULL STACK</strong></p>',1,NULL,5,NULL,'2022-05-13 21:51:30','2022-05-13 21:51:30'),(3,'Combo Kháng Viêm','2 Year','asd',2,'2022-05-14','2022-05-25','<p>ads</p>',1,NULL,4,NULL,'2022-05-13 22:19:19','2022-05-13 22:19:19'),(4,'Combo Kháng Viêmdddd','2 Year','asd',2,'2022-05-14','2022-05-25','<p>asd</p>',1,NULL,5,NULL,'2022-05-13 22:21:27','2022-05-13 22:21:27'),(5,'Combo Kháng Viêm aaa','2 Year','Internship',2,'2022-05-14','2022-05-23','<p>NBote</p>',1,NULL,12,NULL,'2022-05-13 23:26:03','2022-05-13 23:26:03');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skills` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `skills_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (1,'Front End',1,'2022-05-12 21:25:15','2022-05-12 21:25:15'),(2,'Back End',1,'2022-05-12 22:46:11','2022-05-12 22:46:11'),(3,'.NET',1,'2022-05-13 08:40:24','2022-05-13 08:40:24'),(4,'Java',0,'2022-05-14 04:49:44','2022-05-14 04:49:45'),(5,'Full Stack',0,'2022-05-14 04:49:41','2022-05-14 04:49:46'),(6,'HTML',1,'2022-05-13 21:50:09','2022-05-13 21:50:09'),(7,'CSS',1,'2022-05-13 21:50:15','2022-05-13 21:50:15'),(8,'Javascript',1,'2022-05-13 21:50:25','2022-05-13 21:50:25'),(9,'Ajax',1,'2022-05-13 21:50:32','2022-05-13 21:50:32'),(10,'Bootstrap',1,'2022-05-13 21:50:39','2022-05-13 21:50:39'),(11,'Bootstrap 23',1,'2022-05-13 23:06:35','2022-05-13 23:06:35'),(12,'Bootstrap 232',1,'2022-05-13 23:07:21','2022-05-13 23:07:21');
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -165,6 +228,7 @@ CREATE TABLE `users` (
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role_id` int(11) DEFAULT NULL,
+  `group_id` int(11) DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -179,7 +243,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Lê Phát Thời','thoi@gmail.com',NULL,'$2y$10$M4OpF/wEiz6IHm.GHaHBqu0UD8JnSqElbQ0goGE1WH/bBt7hpwFuO',NULL,NULL,'2022-05-12 07:02:05','2022-05-12 07:02:05');
+INSERT INTO `users` VALUES (1,'Thời nè !!','thoi@gmail.com',NULL,'$2y$10$ZthbC7vhWL/OtussUSnSCu7mM7s09oRrffgajNLdEjmScH0dHKsvS',NULL,NULL,NULL,'2022-05-12 07:36:10','2022-05-12 07:36:10');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -192,4 +256,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-14  0:51:45
+-- Dump completed on 2022-05-14 13:40:34
