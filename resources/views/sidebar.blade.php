bfd175e9aa9f5d6fa3e9e7ac98ce705872f086fd
ThoiPT <lephatloc2016@gmail.com>
2022-05-16 16:18:25 +0700

    Update databse and sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -81,30 +81,6 @@
                     </ul>
                 </li>
 
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-search"></i>
-                        <p>
-                            Request
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="/request/create" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>New Request</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/request/list" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Request List</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
